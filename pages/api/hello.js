86340f19f2432d36a6cc9b45653e4314a20a6432
ShikariSohan <moksedur.rahman.sohan@gmail.com>
2022-04-28 16:16:17 +0600

    next app added

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// default. we will chnage this api
 
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
