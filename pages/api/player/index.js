365e5ae8ec3dee5181369b14966eb673847d61d7
unknown <moksedur.rahman.sohan@gmail.com>
2022-08-11 11:39:01 +0600

    player form added

---
+++ pages/api/player/index.js
@@ -0,0 +1,39 @@
+import { executeQuery } from "../../../lib/db";
+const dateFix= (date)=>{
+      const d = date.slice(0,10);
+      return d;
+}
+export default async function (req, res) {
+  if (req.method === "POST") {
+    try {
+        console.log(req.body);
+        const { player ,url} = req.body;
+        console.log(player);
+        ///TODO : ADD Validation
+         const addPlayer = await executeQuery(
+          "INSERT INTO player VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+          [
+            player.p_id,
+            player.teamName,
+            player.name,
+            dateFix(player.dob),
+            player.country,
+            player.jersey_no,
+            player.height,
+            player.release_clause,
+            dateFix(player.join_date),
+            player.position,
+            player.biography,
+            "False",
+            dateFix(player.contract_end),
+            url
+
+          ]
+        );
+        res.status(200).json({ok:"ok"})
+    } catch (err) {
+      console.log(err);
+      res.status(400).json(err);
+    }
+  }
+}
--- pages/editorspanel/index.js
+++ pages/editorspanel/index.js
@@ -3,15 +3,19 @@ import EditorAppBar from "../../components/EditorComponent/editorAppBar";
 import { useState } from "react";
 import { useAuth } from "../../context/AuthProvider";
 import LoadingScreen from "../../components/LoadingScreen";
+import Head from "next/head";
 export default function editorspanel() {
-const { auth, loading, IsEditor } = useAuth();
+  const { auth, loading, IsEditor } = useAuth();
 
-if (loading) return <LoadingScreen />;
-if (!IsEditor) return <h1>The page is not found</h1>;
+  if (loading) return <LoadingScreen />;
+  if (!IsEditor) return <h1>The page is not found</h1>;
   return (
     <>
+      <Head>
+        <title>{`Editor | Team Rivals`}</title>
+      </Head>
       <Navbar />
-      <EditorAppBar/>
+      <EditorAppBar />
     </>
   );
 }
